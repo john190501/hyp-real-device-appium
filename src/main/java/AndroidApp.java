c2849f3aab6664a6d849a51b273530a0f3e24af3
john190501 <150131708+john190501@users.noreply.github.com>
2024-09-20 19:36:28 +0530

    Update AndroidApp.java

--- src/main/java/AndroidApp.java
+++ src/main/java/AndroidApp.java
@@ -32,7 +32,7 @@ public class AndroidApp {
             capabilities.setCapability("platformName", platform);
             capabilities.setCapability("isRealMobile", true);
             //AppURL (Create from Wikipedia.apk sample in project)
-            capabilities.setCapability("app", "lt://proverbial_android"); //Enter your app url
+            capabilities.setCapability("app", "lt://proverbial-android"); //Enter your app url
             capabilities.setCapability("deviceOrientation", "PORTRAIT");
             capabilities.setCapability("console", true);
             capabilities.setCapability("network", false);
